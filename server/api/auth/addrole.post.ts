ae953b814b189f6855d2714aaf6ec5570ec84d5e
NyllRE <nyll.redeast@gmail.com>
2023-02-15 19:50:30 +0300

    added role validation

--- server/api/auth/addrole.post.ts
+++ server/api/auth/addrole.post.ts
@@ -3,8 +3,28 @@ import { addRole } from '../../db/modifySchema';
 
 export default defineEventHandler(async (event) => {
 	const body = await readBody(event);
+	const user = event.context.auth?.user;
 
 	const { role } = body;
 
-	return await addRole(role);
+	console.log(user);
+	if (user.roleName === 'BASIC') {
+		return sendError(
+			event,
+			createError({
+				statusCode: 403,
+				statusMessage: 'you cant modify roles',
+			})
+		);
+	} else if (user.roleName === 'ADMIN') {
+		return await addRole(role);
+	} else {
+		return sendError(
+			event,
+			createError({
+				statusCode: 500,
+				statusMessage: 'Unknown role',
+			})
+		);
+	}
 });
