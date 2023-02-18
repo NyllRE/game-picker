031c6cc3e98dcc8971fe055afac4bde65a32f929
NyllRE <nyll.redeast@gmail.com>
2023-02-19 02:06:31 +0300

    storing but issue with fetched model

--- server/db/games.ts
+++ server/db/games.ts
@@ -23,6 +23,74 @@ function remFromArray(arr: number[], num: any) {
 	return arr;
 }
 
+class InconclusiveError extends Error {
+	constructor(message: any) {
+		super(message);
+		this.name = 'InconclusiveError';
+		this.message = message;
+	}
+}
+
+const saveGame = async (appDetails: any) => {
+	try {
+		const constructedData = {
+			name: appDetails.name,
+			short_description: appDetails.short_description
+				? appDetails.short_description
+				: appDetails.detailed_description,
+			header_image: appDetails.header_image
+				? appDetails.header_image
+				: undefined,
+			website: appDetails.website ? appDetails.website : undefined,
+			developers: {
+				connectOrCreate: appDetails.developers
+					? appDetails.developers.map((developer: string) => ({
+							where: { name: developer },
+							create: { name: developer },
+					  }))
+					: undefined,
+			},
+			publishers: {
+				connectOrCreate: appDetails.publishers
+					? appDetails.publishers.map((publisher: string) => ({
+							where: { name: publisher },
+							create: { name: publisher },
+					  }))
+					: undefined,
+			},
+			genres: {
+				connectOrCreate: appDetails.genres
+					? appDetails.genres.map(
+							(genre: { id: string; description: string }) => ({
+								where: {
+									genreid: Number(genre.id),
+								},
+								create: {
+									genreid: Number(genre.id),
+									name: genre.description,
+								},
+							})
+					  )
+					: undefined,
+			},
+			rating: appDetails.metacritic
+				? appDetails.metacritic.score / 100
+				: undefined,
+		};
+		//=>> Store the game in the Prisma database
+		return await prisma.game.upsert({
+			where: { id: appDetails.steam_appid },
+			update: constructedData,
+			create: {
+				id: appDetails.steam_appid,
+				...constructedData,
+			},
+		});
+	} catch (error) {
+		return `data was not filled correctly ${error}`;
+	}
+};
+
 export const fetchAndStoreGames = async () => {
 	try {
 		// Fetch all games from the Steam API
@@ -154,11 +222,10 @@ export const fetchAndStoreGamesFaster = async (atOnce: number) => {
 		/**
 		 * ### These are the ids that we will then clean up
 		 */
-		let appTrigger: number[] = [1326560, 1326590, 1326650]; //=>> we will collect each ${atOnce} games at one time
-		let fetchedGamesCount = 0;
+		let appTrigger: number[] = []; //=>> we will collect each ${atOnce} games at one time
 
 		/**
-		 * ### These urls does not exist in the API, so we need to fetch them
+		 * ### These urls does not exist in our database, so we need to fetch them
 		 */
 		let urls: string[] = [];
 		//=>> Loop through each game and fetch its data from the Steam API
@@ -167,16 +234,28 @@ export const fetchAndStoreGamesFaster = async (atOnce: number) => {
 				appTrigger.push(app.appid);
 			} else {
 				//=>> create the batch of urls to fetch
+				let alreadyDone = 0;
 				for (const idx in appTrigger) {
+					if (appTrigger[idx] === alreadyDone) return Error('WHAT?');
+					alreadyDone = appTrigger[idx];
 					progress++;
+					// console.log(JSON.stringify(appTrigger[idx]));
+
 					//=>> we have to check if we already fetched this game
 					const exists = await prisma.fetched.findFirst({
 						where: {
 							gameid: Number(app.appid),
 						},
 					});
-					if (exists != null) {
+					const stored = await prisma.game.findFirst({
+						where: {
+							id: Number(app.appid),
+						},
+					});
+
+					if (exists != null || stored != null) {
 						appTrigger = remFromArray(appTrigger, appTrigger[idx]);
+						// console.log(`skipped ${exists.gameid}`);
 						continue;
 					}
 
@@ -185,32 +264,97 @@ export const fetchAndStoreGamesFaster = async (atOnce: number) => {
 						`http://store.steampowered.com/api/appdetails?appids=${appTrigger[idx]}`
 					);
 
-					fetchedGamesCount++;
 					console.log(
-						`${progress} out of ${appList.length} (%${String(
-							(progress / appList.length) * 100
-						).slice(0, 4)}) - new`
+						`(%${String((progress / appList.length) * 100).slice(
+							0,
+							4
+						)}) ${progress}/${appList.length} ${appTrigger[idx]}`
 					);
 
 					//! we will add them to the database later
-					// await prisma.fetched.create({
-					// 	data: {
-					// 		gameid: Number(app.appid),
-					// 	},
-					// });
 				}
 				// appTrigger = urls.length < 10 ? [] : appTrigger; //=>> Reset the array if it's 10 or greater
-				if (urls.length <= atOnce) continue;
-				console.log(fetchedGamesCount, urls);
+				if (urls.length < atOnce) {
+					continue;
+				}
 
 				console.log(`just got ${urls.length} urls\n`);
 
 				const promises = urls.map((url) =>
-					fetch(url).then((response) => response.json())
+					fetch(url)
+						.then((response): {} => response.json())
+						.then(async (app: any) => {
+							// console.log(JSON.stringify(app));
+							const entries = Object.entries(app);
+							const [steam_appid, appData] = entries[0];
+							const appDetails = appData.data;
+
+							const status: boolean = appData.success;
+							try {
+								console.log(steam_appid, status);
+								//=>> the saveGame function saves it, we put it to the completedData array just for a response
+								if (!status) return status; //=> skipping if fetching failed
+								const game = await saveGame({
+									steam_appid,
+									...appDetails,
+								}).catch((error) => error.message);
+								console.log(`is the error here: ${JSON.stringify(game.id)}`);
+
+								return game;
+							} catch (e) {
+								console.error(`data was incomplete ${JSON.stringify(e)}`);
+								return e;
+							}
+							// return app;
+						})
 				);
 				const data = await Promise.all(promises)
-					.then((data) => {
-						// data will be an array containing the JSON data from each URL
+					.then(async (data: object[]) => {
+						// //=>> data will be an array containing the JSON data from each URL
+
+						for (let game in data) {
+							game = data[game];
+							console.log(game.id);
+
+							console.dir(game);
+
+							const fetched = await prisma.fetched.create({
+								data: {
+									gameid: Number(game.id),
+								},
+							});
+							console.dir(fetched);
+						}
+						// let completedData = []; //=> only for response purposes
+
+						// let appDetailsResponse: any;
+						// for (appDetailsResponse in data) {
+						// 	appDetailsResponse = data[appDetailsResponse];
+
+						// 	let app: { data: object; success: boolean } | number | any;
+						// 	for (app in appDetailsResponse) {
+						// 		app = appDetailsResponse[app];
+
+						// 		// console.log(app, appDetailsResponse);
+						// 		const appDetails = app.data;
+						// 		const status: boolean = app.success;
+						// 		const doneStoring = await prisma.fetched.create({
+						// 			data: {
+						// 				gameid: Number(appDetails.steam_appid),
+						// 			},
+						// 		});
+						// 		console.log('done storing: ', doneStoring);
+						// 		try {
+						// 			//=>> the saveGame function saves it, we put it to the completedData array just for a response
+						// 			if (!status) continue; //=> skipping if fetching failed
+						// 			completedData.push(
+						// 				await saveGame(appDetails).catch((error) => error)
+						// 			);
+						// 		} catch (e) {
+						// 			console.error('data was incomplete');
+						// 		}
+						// 	}
+						// }
 						return data;
 					})
 					.catch((error) => {
@@ -218,10 +362,6 @@ export const fetchAndStoreGamesFaster = async (atOnce: number) => {
 					});
 
 				return data;
-				// return appDetailsResponse;
-
-				// const appDetails = appDetailsResponse[app.appid].data;
-				// const status: boolean = appDetailsResponse[app.appid].success;
 			}
 		}
 	} catch (error) {
